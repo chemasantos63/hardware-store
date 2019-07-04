e134640236d53c0f0b762ff139bd7ebaed63f143
Chema Santos <chemasantos63@gmail.com>
2019-07-04 08:56:21 -0600

    adding mongo connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,37 @@
 import { ApolloServer } from "apollo-server-express"
 import express from "express"
+import  mongoose  from "mongoose"
 import typeDefs from "./typeDefs"
 import resolvers from "./resolvers"
+import { 
+    APP_PORT, IN_PROD, DB_USERNAME, DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT, 
+} from "../config"
 
-const {
-    APP_PORT = 4000,
-    NODE_ENV = "development"
-} = process.env
+(async () => {
+    try {
+       await mongoose.connect(
+            `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+            { useNewUrlParser:true }
+        )
+        
+        const app = express()
+        
+        app.disable("x-powered-by")
+        
+        const server = new ApolloServer({
+            typeDefs,
+            resolvers,
+            playground: IN_PROD
+        })
+        
+        server.applyMiddleware({ app })
+        
+        app.listen({ port: APP_PORT }, () =>
+            console.log(`http://localhost:${APP_PORT}${server.graphqlPath}`)
+        )
+        
+    } catch (e) {
+        console.error(e)
+    }
+})()
 
-const IN_PROD = NODE_ENV !== "production"
-
-const app = express()
-
-app.disable("x-powered-by")
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    playground: IN_PROD
-})
-
-server.applyMiddleware({ app })
-
-app.listen({port: APP_PORT}, () => 
-    console.log(`http://localhost:${APP_PORT}${server.graphqlPath}`)
-)
