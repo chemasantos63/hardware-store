f78bcc823ee3d42d55d19d3d72f9586f3392853a
Chema Santos <chemasantos63@gmail.com>
2019-07-05 02:16:03 -0600

    adding validations

---
+++ backend/src/schemas/user.ts
@@ -0,0 +1,20 @@
+import Joi from "joi"
+
+export default Joi.object().keys({
+    email: Joi.string().email().required().label("Email"),
+    username: Joi.string().alphanum().min(4).max(30).required().label("Username"),
+    name: Joi.string().max(254).required().label("Name"),
+    password: Joi.string()
+    .regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d).*$/)
+    .required().label("Password")
+    .options({
+        language: {
+            string: {
+                regex: {
+                    base: 
+                    "must have at least one lowercase letter, one uppercase letter, one digit, and one special character"
+                }
+            }
+        }
+    })
+})
