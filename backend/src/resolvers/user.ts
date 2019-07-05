c34b220c14b750c4880fe92603c7dadf4e422229
Chema Santos <chemasantos63@gmail.com>
2019-07-05 00:59:38 -0600

    user persistences added

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -1,15 +1,31 @@
+import mongoose from "mongoose"
+import { UserInputError } from "apollo-server-express"
+import { User } from "../models"
+
 export default {
     Query: {
         users: (root, args, context, info) => {
+            //TODO: auth, projection, pagination,sanitization
+
+            return User.find({});
 
         },
-        user: (root, args, context, info) => {
+        user: (root, { id }, context, info) => {
+            //TODO: auth,projection,sanitization
+
+            if (!mongoose.Types.ObjectId.isValid(id)) {
+                throw new UserInputError(`${id} is not a valid user ID.`)
+            }
+
+            return User.findById(id);
 
         }
     },
     Mutation: {
         singUp: (root, args, context, info) => {
-
+            //TODO no auth
+            //validation
+            return User.create(args)
         }
     }
 }
