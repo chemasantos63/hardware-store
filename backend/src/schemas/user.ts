b2231cc6153f7ea264f0139a0352cbcb2ef91319
Chema Santos <chemasantos63@gmail.com>
2019-07-05 02:52:46 -0600

    some changes

--- backend/src/schemas/user.ts
+++ backend/src/schemas/user.ts
@@ -12,7 +12,7 @@ export default Joi.object().keys({
             string: {
                 regex: {
                     base: 
-                    "must have at least one lowercase letter, one uppercase letter, one digit, and one special character"
+                    "must have at least one lowercase letter, one uppercase letter, and one digit."
                 }
             }
         }
