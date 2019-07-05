1fe22080deac3d17ccf044af1b4b1f9783d7e74c
Chema Santos <chemasantos63@gmail.com>
2019-07-04 20:15:44 -0600

    finish user model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,5 +1,12 @@
 import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
-    
+    email:String,
+    username:String,
+    name:String,
+    password:String
+},{
+    timestamps:true
 })
+
+export default mongoose.model('User',userSchema)
